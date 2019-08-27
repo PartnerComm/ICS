2b8abb02a90a43ff786faf79bb8127930d2c213e
Jason Crane <jcrane@partnercomm.net>
2019-08-27 17:24:03 -0500

    Don't check for timezone when in timezone block
    That was breaking the event timezone in Outlook. Believe this should fix it, testing and should clean up.

--- src/CalendarExport.php
+++ src/CalendarExport.php
@@ -57,13 +57,13 @@ class CalendarExport
         foreach ($this->getCalendars() as $cal) {
             //start calendar
             $this->stream->addItem('BEGIN:VCALENDAR')
-                ->addItem('VERSION:'.$cal->getVersion())
-                ->addItem('PRODID:'.$cal->getProdId())
-                ->addItem('CALSCALE:'.$cal->getCalendarScale())
-                ->addItem('METHOD:'.$cal->getMethod());
+                ->addItem('VERSION:' . $cal->getVersion())
+                ->addItem('PRODID:' . $cal->getProdId())
+                ->addItem('CALSCALE:' . $cal->getCalendarScale())
+                ->addItem('METHOD:' . $cal->getMethod());
 
             if ($cal->getName()) {
-                $this->stream->addItem('NAME:'.$cal->getName());
+                $this->stream->addItem('NAME:' . $cal->getName());
             }
 
             if ($cal->getImage()) {
@@ -72,7 +72,7 @@ class CalendarExport
 
             //custom headers
             foreach ($cal->getCustomHeaders() as $key => $value) {
-                $this->stream->addItem($key.':'.$value);
+                $this->stream->addItem($key . ':' . $value);
             }
 
             //timezone
@@ -94,29 +94,29 @@ class CalendarExport
             $transitions = $tz->getTransitions(strtotime($startYear . '-01-01'), strtotime($endYear . '-12-31'));
 
             $daylightSavings = array(
-                        'exists' => false,
-                        'start' => '',
-                        'offsetTo' => '',
-                        'offsetFrom' => ''
-                    );
+                'exists' => false,
+                'start' => '',
+                'offsetTo' => '',
+                'offsetFrom' => ''
+            );
 
             $standard = array(
-                        'start' => '',
-                        'offsetTo' => '',
-                        'offsetFrom' => ''
-                    );
+                'start' => '',
+                'offsetTo' => '',
+                'offsetFrom' => ''
+            );
 
             foreach ($transitions as $transition) {
                 $varName = ($transition['isdst']) ? 'daylightSavings' : 'standard';
 
                 ${$varName}['exists'] = true;
-                if ($this->dateTimeFormat === 'local') {
-                    ${$varName}['start'] = ':' . $this->formatter->getFormattedDateTime(new \DateTime($transition['time']));
-                } else if ($this->dateTimeFormat === 'utc') {
-                    ${$varName}['start'] = ':' . $this->formatter->getFormattedUTCDateTime(new \DateTime($transition['time']));
-                } else if ($this->dateTimeFormat == 'local-tz') {
-                    ${$varName}['start'] = ';' . $this->formatter->getFormattedDateTimeWithTimeZone(new \DateTime($transition['time']));
-                }
+                // if ($this->dateTimeFormat === 'local') {
+                ${$varName}['start'] = ':' . $this->formatter->getFormattedDateTime(new \DateTime($transition['time']));
+                // } else if ($this->dateTimeFormat === 'utc') {
+                //     ${$varName}['start'] = ':' . $this->formatter->getFormattedUTCDateTime(new \DateTime($transition['time']));
+                // } else if ($this->dateTimeFormat == 'local-tz') {
+                //     ${$varName}['start'] = ';' . $this->formatter->getFormattedDateTimeWithTimeZone(new \DateTime($transition['time']));
+                // }
 
                 ${$varName}['offsetTo'] = $this->formatter->getFormattedTimeOffset($transition['offset']);
 
@@ -129,12 +129,12 @@ class CalendarExport
                 ${$varName}['offsetFrom'] = $this->formatter->getFormattedTimeOffset($offset);
             }
 
-            $this->stream->addItem('TZID:'.$tz->getName());
+            $this->stream->addItem('TZID:' . $tz->getName());
 
             $this->stream->addItem('BEGIN:STANDARD')
-                    ->addItem('DTSTART'.$standard['start'])
-                    ->addItem('TZOFFSETTO:'.$standard['offsetTo'])
-                    ->addItem('TZOFFSETFROM:'.$standard['offsetFrom']);
+                ->addItem('DTSTART' . $standard['start'])
+                ->addItem('TZOFFSETTO:' . $standard['offsetTo'])
+                ->addItem('TZOFFSETFROM:' . $standard['offsetFrom']);
 
             if ($daylightSavings['exists']) {
                 $this->stream->addItem('RRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU');
@@ -143,10 +143,10 @@ class CalendarExport
 
             if ($daylightSavings['exists']) {
                 $this->stream->addItem('BEGIN:DAYLIGHT')
-                        ->addItem('DTSTART'.$daylightSavings['start'])
-                        ->addItem('TZOFFSETTO:'.$daylightSavings['offsetTo'])
-                        ->addItem('TZOFFSETFROM:'.$daylightSavings['offsetFrom'])
-                        ->addItem('RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU')
+                    ->addItem('DTSTART' . $daylightSavings['start'])
+                    ->addItem('TZOFFSETTO:' . $daylightSavings['offsetTo'])
+                    ->addItem('TZOFFSETFROM:' . $daylightSavings['offsetFrom'])
+                    ->addItem('RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU')
                     ->addItem('END:DAYLIGHT');
             }
 
@@ -165,79 +165,79 @@ class CalendarExport
                 } else if ($this->dateTimeFormat === 'utc') {
                     $dtStart = ':' . $this->formatter->getFormattedUTCDateTime($event->getStart());
                     $dtEnd = ':' . $this->formatter->getFormattedUTCDateTime($event->getEnd());
-                } else if ($this->dateTimeFormat === 'local-tz'){
+                } else if ($this->dateTimeFormat === 'local-tz') {
                     $dtStart = ';' . $this->formatter->getFormattedDateTimeWithTimeZone($event->getStart());
                     $dtEnd = ';' . $this->formatter->getFormattedDateTimeWithTimeZone($event->getEnd());
                 }
 
                 $this->stream->addItem('BEGIN:VEVENT')
-                    ->addItem('UID:'.$event->getUid())
-                    ->addItem('DTSTART'. $dtStart)
-                    ->addItem('DTEND'. $dtEnd);
+                    ->addItem('UID:' . $event->getUid())
+                    ->addItem('DTSTART' . $dtStart)
+                    ->addItem('DTEND' . $dtEnd);
 
                 if ($event->getRecurrenceRule() instanceof RecurrenceRule) {
                     $this->stream->addItem($event->getRecurrenceRule()->__toString());
                 }
 
                 foreach ($event->getExceptionDates() as $date) {
-                    $this->stream->addItem('EXDATE:'.$this->formatter->getFormattedDateTime($date));
+                    $this->stream->addItem('EXDATE:' . $this->formatter->getFormattedDateTime($date));
                 }
 
                 if ($event->getSequence()) {
-                    $this->stream->addItem('SEQUENCE:'.$event->getSequence());
+                    $this->stream->addItem('SEQUENCE:' . $event->getSequence());
                 }
 
                 if ($event->getTransp()) {
-                    $this->stream->addItem('TRANSP:'.$event->getTransp());
+                    $this->stream->addItem('TRANSP:' . $event->getTransp());
                 }
 
                 if ($event->getImage()) {
                     $this->stream->addItem($this->formatter->getFormattedImageString($event->getImage()));
                 }
 
-                $this->stream->addItem('STATUS:'.$event->getStatus())
-                    ->addItem('SUMMARY:'.$event->getSummary())
-                    ->addItem('DESCRIPTION:'.$this->formatter->getEscapedText($event->getDescription()));
+                $this->stream->addItem('STATUS:' . $event->getStatus())
+                    ->addItem('SUMMARY:' . $event->getSummary())
+                    ->addItem('DESCRIPTION:' . $this->formatter->getEscapedText($event->getDescription()));
 
                 if ($event->getClass()) {
-                    $this->stream->addItem('CLASS:'.$event->getClass());
+                    $this->stream->addItem('CLASS:' . $event->getClass());
                 }
 
                 /* @var $location Location */
                 foreach ($event->getLocations() as $location) {
                     $this->stream
-                            ->addItem('LOCATION'.$location->getUri().$location->getLanguage().':'.$this->formatter->getEscapedText($location->getName()));
+                        ->addItem('LOCATION' . $location->getUri() . $location->getLanguage() . ':' . $this->formatter->getEscapedText($location->getName()));
                 }
 
                 if ($event->getPriority() > 0 && $event->getPriority() <= 9) {
-                    $this->stream->addItem('PRIORITY:'.$event->getPriority());
+                    $this->stream->addItem('PRIORITY:' . $event->getPriority());
                 }
 
                 if ($event->getGeo()) {
-                    $this->stream->addItem('GEO:'.$event->getGeo()->getLatitude().';'.$event->getGeo()->getLongitude());
+                    $this->stream->addItem('GEO:' . $event->getGeo()->getLatitude() . ';' . $event->getGeo()->getLongitude());
                 }
 
                 if ($event->getUrl()) {
-                    $this->stream->addItem('URL:'.$event->getUrl());
+                    $this->stream->addItem('URL:' . $event->getUrl());
                 }
 
 
                 if ($event->getTimestamp()) {
-                    $this->stream->addItem('DTSTAMP:'.$this->formatter->getFormattedUTCDateTime($event->getTimestamp()));
+                    $this->stream->addItem('DTSTAMP:' . $this->formatter->getFormattedUTCDateTime($event->getTimestamp()));
                 } else {
-                    $this->stream->addItem('DTSTAMP:'.$this->formatter->getFormattedUTCDateTime(new \DateTime()));
+                    $this->stream->addItem('DTSTAMP:' . $this->formatter->getFormattedUTCDateTime(new \DateTime()));
                 }
 
                 if ($event->getCreated()) {
-                    $this->stream->addItem('CREATED:'.$this->formatter->getFormattedUTCDateTime($event->getCreated()));
+                    $this->stream->addItem('CREATED:' . $this->formatter->getFormattedUTCDateTime($event->getCreated()));
                 }
 
                 if ($event->getLastModified()) {
-                    $this->stream->addItem('LAST-MODIFIED:'.$this->formatter->getFormattedUTCDateTime($event->getLastModified()));
+                    $this->stream->addItem('LAST-MODIFIED:' . $this->formatter->getFormattedUTCDateTime($event->getLastModified()));
                 }
 
                 foreach ($event->getAttendees() as $attendee) {
-                    $this->stream->addItem((string)$attendee);
+                    $this->stream->addItem((string) $attendee);
                 }
 
                 if ($event->getOrganizer()) {
@@ -248,39 +248,39 @@ class CalendarExport
                 foreach ($event->getAlarms() as $alarm) {
                     //basic requirements for all types of alarm
                     $this->stream->addItem('BEGIN:VALARM')
-                            ->addItem('TRIGGER;VALUE=DATE-TIME:'.$this->formatter->getFormattedUTCDateTime($alarm->getTrigger()))
-                            ->addItem('ACTION:'.$alarm->getAction());
+                        ->addItem('TRIGGER;VALUE=DATE-TIME:' . $this->formatter->getFormattedUTCDateTime($alarm->getTrigger()))
+                        ->addItem('ACTION:' . $alarm->getAction());
 
                     //only handle repeats if both repeat and duration are set
                     if ($alarm->getRepeat() && $alarm->getDuration()) {
-                        $this->stream->addItem('REPEAT:'.$alarm->getRepeat())
-                                ->addItem('DURATION:'.$this->formatter->getFormattedDateInterval($alarm->getDuration()));
+                        $this->stream->addItem('REPEAT:' . $alarm->getRepeat())
+                            ->addItem('DURATION:' . $this->formatter->getFormattedDateInterval($alarm->getDuration()));
                     }
 
                     //action specific logic
                     switch ($alarm->getAction()) {
-                            case 'AUDIO':
-                                $attachments = $alarm->getAttachments();
-                                $this->stream->addItem('ATTACH;'.$attachments[0]);
-                                break;
-                            case 'DISPLAY':
-                                $this->stream->addItem('DESCRIPTION:'.$alarm->getDescription());
-                                break;
-                            case 'EMAIL':
-                                $this->stream->addItem('SUMMARY:'.$alarm->getSummary())
-                                    ->addItem('DESCRIPTION:'.$alarm->getDescription());
+                        case 'AUDIO':
+                            $attachments = $alarm->getAttachments();
+                            $this->stream->addItem('ATTACH;' . $attachments[0]);
+                            break;
+                        case 'DISPLAY':
+                            $this->stream->addItem('DESCRIPTION:' . $alarm->getDescription());
+                            break;
+                        case 'EMAIL':
+                            $this->stream->addItem('SUMMARY:' . $alarm->getSummary())
+                                ->addItem('DESCRIPTION:' . $alarm->getDescription());
 
-                                foreach ($alarm->getAttendees() as $attendee) {
-                                    $this->stream->addItem((string)$attendee);
-                                }
-                                foreach ($alarm->getAttachments() as $attachment) {
-                                    $this->stream->addItem('ATTACH;'.$attachment);
-                                }
-                                break;
-                            default:
-                                throw new \Exception("Unknown ALARM action: '{$alarm->getAction()}'");
-                                break;
-                        }
+                            foreach ($alarm->getAttendees() as $attendee) {
+                                $this->stream->addItem((string) $attendee);
+                            }
+                            foreach ($alarm->getAttachments() as $attachment) {
+                                $this->stream->addItem('ATTACH;' . $attachment);
+                            }
+                            break;
+                        default:
+                            throw new \Exception("Unknown ALARM action: '{$alarm->getAction()}'");
+                            break;
+                    }
 
                     $this->stream->addItem('END:VALARM');
                 }
@@ -327,7 +327,6 @@ class CalendarExport
             throw new \Exception('Invalid Format Chosen');
         }
         return $this;
-
     }
 
     /**
